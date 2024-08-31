b9927a02f7b89a32032f46df401e9db49807e264
Rohit singh <164492896+yasuo72@users.noreply.github.com>
2024-08-31 18:29:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, jsonify
+
+from chat import get_response
+
+app = Flask(__name__)
+@app.get("/")
+def index_get():
+    return render_template("base.html")
+
+@app.post("/predict")
+def predict():
+    text = request.get_json().get("message")
+    response = get_response(text)
+    massage = {"answer": response}
+    return jsonify(massage)
+
+if __name__ == "__main__":
+    app.run(debug=True)
